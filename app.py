b493698c30b3cd3cab1bf9ece1295fba7af86205
teryaspb <ya.terentos@yandex.ru>
2019-09-02 14:52:32 +0300

    xren

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ from flask_restful import Api
 from flask_jwt import JWT
 
 from security import authenticate, identity
-from resources.user import UserRegister
-from resources.item import Item, ItemList
-from resources.store import Store, StoreList
+from code.resources.user import UserRegister
+from code.resources.item import Item, ItemList
+from code.resources.store import Store, StoreList
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
